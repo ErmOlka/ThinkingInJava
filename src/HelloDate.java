c54994a79cc3ac337d8486ce809b9a4ac8112f44
ErmOlka <clipsa.zla@gmail.com>
2013-12-25 00:06:44 +0400

    1

---
+++ src/HelloDate.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+//: Object/Documentation.java
+/** Комментарий к классу */
+public class HelloDate {
+	/** Комментарий к методу */
+	public static void main(String[] args) {
+		System.out.println("Привет! Сегодня: ");
+		System.out.println(new Date());
+	}
+} ///:~
